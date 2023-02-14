9019539dca3498b3e0a78317f874385ee653d5ae
Rha02 <romuzus08@gmail.com>
2023-02-14 11:37:48 -0800

    Created Frontend Template

---
+++ src/index.tsx
@@ -0,0 +1,35 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import reportWebVitals from './reportWebVitals';
+import {Routes, Route, BrowserRouter} from 'react-router-dom'
+import routes from './routes/routes'
+
+import Home from './views/pages/Home'
+import About from './views/pages/About'
+import Calculator from './views/pages/Calculator'
+
+function App() {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path={routes.home} element={<Home />} />
+        <Route path={routes.about} element={<About />} />
+        <Route path={routes.calculator} element={<Calculator />} />
+      </Routes>
+    </BrowserRouter>
+  );
+}
+
+const root = ReactDOM.createRoot(
+  document.getElementById('root') as HTMLElement
+);
+root.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
+
+// If you want to start measuring performance in your app, pass a function
+// to log results (for example: reportWebVitals(console.log))
+// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+reportWebVitals();
