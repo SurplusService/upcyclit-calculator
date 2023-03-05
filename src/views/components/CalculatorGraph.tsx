da23e8efb50520eb2343795a4328e9951247ed7e
Rha02 <romuzus08@gmail.com>
2023-03-04 21:32:29 -0800

    Added charts for Energy consumption and Methane production

--- src/views/components/CalculatorGraph.tsx
+++ src/views/components/CalculatorGraph.tsx
@@ -1,6 +1,18 @@
 import { ResponsiveContainer, PieChart, Pie, Tooltip, Cell } from "recharts"
 import { CalculationDetails } from "./CalculatorInput"
-import { Grid, Typography } from "@mui/material"
+import { Grid, Typography, IconButton, Box } from "@mui/material"
+import { ArrowBackIosNew, ArrowForwardIos } from "@mui/icons-material"
+import { useState } from "react"
+
+/**COLORS is the colors for each category. The index of the array is the category id. */
+const COLORS = ['#0088FE', '#00C49F', '#FFBB28', '#FF8042'];
+
+/**CONVERSIONS is the conversion factors for each modifier. The index of the array is the modifier id. 
+ * For example, the conversion factor for the Quantity modifier is 1.
+ * For gaylords, the conversion factor is 2.5.
+ * For weight, the conversion factor is 0.8.
+*/
+const CONVERSIONS = [1, 0.8, 2.5]
 
 /**CalculatorGraphProps is the props for the CalculatorGraph component. */
 interface CalculatorGraphProps {
@@ -17,17 +29,32 @@ interface PieData {
     value: number
 }
 
-/**COLORS is the colors for each category. The index of the array is the category id. */
-const COLORS = ['#0088FE', '#00C49F', '#FFBB28', '#FF8042'];
+/**CarbonPieChartProps is the props for the CarbonPieChart component. */
+interface CustomPieChartProps {
+    entries: PieData[]
+}
 
-/**CONVERSIONS is the conversion factors for each modifier. The index of the array is the modifier id. 
- * For example, the conversion factor for the Quantity modifier is 1.
- * For gaylords, the conversion factor is 2.5.
- * For weight, the conversion factor is 0.8.
-*/
-const CONVERSIONS = [1, 0.8, 2.5]
+/**CarbonPieChart is a customizable template for pie chart. */
+const CustomPieChart = (props: CustomPieChartProps) => {
+    return (
+        <Grid item xs={12}>
+            <ResponsiveContainer width="100%" height={200}>
+                <PieChart>
+                    <Pie dataKey="value" data={props.entries} innerRadius="40%" fill="#82ca9d">
+                        {props.entries.map((_entry, index) => (
+                            <Cell key={`cell-${index}`} fill={COLORS[index % COLORS.length]} />
+                        ))}
+                    </Pie>
+                    <Tooltip />
+                </PieChart>
+            </ResponsiveContainer>
+        </Grid>
+    )
+}
 
 const CalculatorGraph = (props: CalculatorGraphProps) => {
+    const [selectedChart, setSelectedChart] = useState(0)
+
     // convert all items to the same unit of measurement
     const formattedItems = props.items.map((item) => {
         return {
@@ -36,50 +63,95 @@ const CalculatorGraph = (props: CalculatorGraphProps) => {
         }
     })
 
-    // Calculate the total carbon footprint, energy consumption, and methane production.
-    const totalCarbonFootprint = formattedItems.reduce((total, item) => {
-        return total + item.value * item.category.carbon_footprint
-    }, 0)
+    interface ChartData {
+        name: string
+        total: number
+        entries: PieData[]
+    }
 
-    // Calculate the carbon impact for each category.
-    const carbonImpact: PieData[] = formattedItems.map((item) => {
-        return {
-            name: item.category.name,
-            value: item.value * item.category.carbon_footprint
+    /**charts is the array of information needed for each chart. */
+    const charts: ChartData[] = [
+        {
+            name: "Carbon Footprint",
+            total: formattedItems.reduce((total, item) => {
+                return total + item.value * item.category.carbon_footprint
+            }, 0),
+            entries: formattedItems.map((item) => {
+                return {
+                    name: item.category.name,
+                    value: item.value * item.category.carbon_footprint
+                }
+            })
+        },
+        {
+            name: "Energy Consumption",
+            total: formattedItems.reduce((total, item) => {
+                return total + item.value * item.category.energy_consumption
+            }, 0),
+            entries: formattedItems.map((item) => {
+                return {
+                    name: item.category.name,
+                    value: item.value * item.category.energy_consumption
+                }
+            })
+        },
+        {
+            name: "Methane Production",
+            total: formattedItems.reduce((total, item) => {
+                return total + item.value * item.category.methane_production
+            }, 0),
+            entries: formattedItems.map((item) => {
+                return {
+                    name: item.category.name,
+                    value: item.value * item.category.methane_production
+                }
+            })
         }
-    })
+    ]
+
+    /**handleBackButton is the callback function for the back button. It switched to previous chart */
+    const handleBackButton = () => {
+        if (selectedChart > 0) {
+            setSelectedChart(selectedChart - 1)
+        }
+    }
+
+    /**handleNextButton is the callback function for the next button. It switches to next chart */
+    const handleNextButton = () => {
+        if (selectedChart < charts.length - 1) {
+            setSelectedChart(selectedChart + 1)
+        }
+    }
 
     return (
         <div>
-            <Grid container spacing={2} maxWidth="sm" mt={0}>
+            <Grid container spacing={0} maxWidth="sm" mt={2}>
                 {props.items.length > 0 ?
                     <Grid item xs={12}>
-                        <Grid item xs={12}>
-                            <Typography variant="h6" color="text.secondary" align="center">
-                                Total Carbon Footprint: {totalCarbonFootprint}
+                        <Box sx={{ display: "flex", flexDirection: "row", justifyContent: "space-between"}} alignContent="center">
+                            <IconButton aria-label="back" onClick={handleBackButton} disabled={selectedChart-1 < 0}>
+                                <ArrowBackIosNew fontSize="small" />
+                            </IconButton>
+
+                            <Typography variant="h6" color="text.secondary">
+                                Total {charts[selectedChart].name}: {charts[selectedChart].total.toFixed(2)} 
                             </Typography>
-                        </Grid>
-                        <Grid item xs={12}>
-                            <ResponsiveContainer width="100%" height={200}>
-                                <PieChart>
-                                    <Pie dataKey="value" data={carbonImpact} innerRadius="40%" fill="#82ca9d">
-                                        {carbonImpact.map((_entry, index) => (
-                                            <Cell key={`cell-${index}`} fill={COLORS[index % COLORS.length]} />
-                                        ))}
-                                    </Pie>
-                                    <Tooltip />
-                                </PieChart>
-                            </ResponsiveContainer>
-                        </Grid>
+                            
+                            <IconButton aria-label="next" onClick={handleNextButton} disabled={selectedChart+1 >= charts.length}>
+                                <ArrowForwardIos fontSize="small" />
+                            </IconButton>
+                        </Box>
                     </Grid>
                     :
-
                     <Grid item xs={12}>
                         <Typography variant="h6" color="text.secondary" align="center">
-                            Enter some values to see a graph.
+                            Enter some items to see the carbon footprint.
                         </Typography>
                     </Grid>
                 }
+                <Grid item xs={12}>
+                    <CustomPieChart entries={charts[selectedChart].entries} />
+                </Grid>
             </Grid>
         </div>
     )
