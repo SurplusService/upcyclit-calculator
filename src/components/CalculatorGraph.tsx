fcf6eb50f8362e7db86896846d283d82bfbe4ef2
Rha02 <romuzus08@gmail.com>
2023-04-05 10:57:24 -0700

    Displaying units on chart tooltips

--- src/components/CalculatorGraph.tsx
+++ src/components/CalculatorGraph.tsx
@@ -1,11 +1,19 @@
 import { ResponsiveContainer, PieChart, Pie, Tooltip, Cell } from "recharts"
 import { CalculationDetails } from "./CalculatorInput"
-import { Grid, Typography, IconButton, Box } from "@mui/material"
+import { Grid, Typography, IconButton, Box, Card } from "@mui/material"
 import { ArrowBackIosNew, ArrowForwardIos } from "@mui/icons-material"
 import { useState } from "react"
 
+
 /**COLORS is the colors for each category. The index of the array is the category id. */
 const COLORS = ['#0088FE', '#00C49F', '#FFBB28', '#FF8042'];
+
+/**units is the units for each category. The index of the array is the category id. */
+const units = {
+    carbonFootprint: 'kg CO2e',
+    energyConsumption: 'kWh',
+    methaneProduction: 'kg CH4',
+}
 
 /**CONVERSIONS is the conversion factors for each modifier. The index of the array is the modifier id. 
  * For example, the conversion factor for the Quantity modifier is 1.
@@ -13,6 +21,64 @@ const COLORS = ['#0088FE', '#00C49F', '#FFBB28', '#FF8042'];
  * For weight, the conversion factor is 0.8.
 */
 const CONVERSIONS = [1, 0.8, 2.5]
+
+/**round() takes a number and rounds it to 2 decimal points */
+const round = (num: number) => Math.round(num * 100) / 100
+
+/**PieData models data for a single pie in the pie chart. */
+interface PieData {
+    name: string
+    value: number
+}
+
+/**
+ * ChartData is holds all data for the pie chart.
+ */
+interface ChartData {
+    name: string
+    unit: string
+    total: number
+    entries: PieData[]
+}
+
+/**CarbonPieChartProps is the props for the CarbonPieChart component. */
+interface CustomPieChartProps {
+    chart: ChartData
+}
+
+/**CarbonPieChart is a customizable template for pie chart. */
+const CustomPieChart = (props: CustomPieChartProps) => {
+    let unit = props.chart.unit
+    let entries = props.chart.entries
+
+    /**PieChartTooltip is the custom tooltip for the pie chart. */
+    const PieChartTooltip = ({ active, payload, label }: any) => {
+        if (active && payload && payload.length) {
+            return (
+                <Card>
+                    <p className="label" style={{ margin: "10px" }}>{`${payload[0].name}: ${payload[0].value} ${unit} `}</p>
+                </Card>
+            );
+        }
+
+        return null;
+    }
+
+    return (
+        <Grid item xs={12}>
+            <ResponsiveContainer width="100%" height={400}>
+                <PieChart>
+                    <Pie dataKey="value" data={entries} fill="#82ca9d">
+                        {entries.map((_entry, index) => (
+                            <Cell key={`cell-${index}`} fill={COLORS[index % COLORS.length]} />
+                        ))}
+                    </Pie>
+                    <Tooltip content={PieChartTooltip} />
+                </PieChart>
+            </ResponsiveContainer>
+        </Grid>
+    )
+}
 
 /**CalculatorGraphProps is the props for the CalculatorGraph component. */
 interface CalculatorGraphProps {
@@ -23,37 +89,7 @@ interface CalculatorGraphProps {
     items: CalculationDetails[]
 }
 
-/**PieData is models a data for the pie chart. */
-interface PieData {
-    name: string
-    value: number
-}
-
-/**CarbonPieChartProps is the props for the CarbonPieChart component. */
-interface CustomPieChartProps {
-    entries: PieData[]
-}
-
-/**CarbonPieChart is a customizable template for pie chart. */
-const CustomPieChart = (props: CustomPieChartProps) => {
-    return (
-        <Grid item xs={12}>
-            <ResponsiveContainer width="100%" height={400}>
-                <PieChart>
-                    <Pie dataKey="value" data={props.entries} fill="#82ca9d">
-                        {props.entries.map((_entry, index) => (
-                            <Cell key={`cell-${index}`} fill={COLORS[index % COLORS.length]} />
-                        ))}
-                    </Pie>
-                    <Tooltip />
-                </PieChart>
-            </ResponsiveContainer>
-        </Grid>
-    )
-}
-
-const round = (num: number) => Math.round(num * 100) / 100
-
+/**CalculatorGraph is the component for the calculator graph. */
 const CalculatorGraph = (props: CalculatorGraphProps) => {
     const [selectedChart, setSelectedChart] = useState(0)
 
@@ -65,18 +101,11 @@ const CalculatorGraph = (props: CalculatorGraphProps) => {
         }
     })
 
-    interface ChartData {
-        name: string
-        unit: string
-        total: number
-        entries: PieData[]
-    }
-
     /**charts is the array of information needed for each chart. */
     const charts: ChartData[] = [
         {
             name: "Carbon Footprint",
-            unit: "kg CO2e",
+            unit: units.carbonFootprint,
             total: formattedItems.reduce((total, item) => {
                 return total + item.value * item.category.carbon_footprint
             }, 0),
@@ -89,7 +118,7 @@ const CalculatorGraph = (props: CalculatorGraphProps) => {
         },
         {
             name: "Energy Consumption",
-            unit: "kWh",
+            unit: units.energyConsumption,
             total: formattedItems.reduce((total, item) => {
                 return total + item.value * item.category.energy_consumption
             }, 0),
@@ -102,7 +131,7 @@ const CalculatorGraph = (props: CalculatorGraphProps) => {
         },
         {
             name: "Methane Production",
-            unit: "kg CH4",
+            unit: units.methaneProduction,
             total: formattedItems.reduce((total, item) => {
                 return total + item.value * item.category.methane_production
             }, 0),
@@ -133,16 +162,16 @@ const CalculatorGraph = (props: CalculatorGraphProps) => {
         <Grid container spacing={0} maxWidth="sm" mt={2}>
             {props.items.length > 0 ?
                 <Grid item xs={12}>
-                    <Box sx={{ display: "flex", flexDirection: "row", justifyContent: "space-between"}} alignContent="center">
-                        <IconButton aria-label="back" onClick={handleBackButton} disabled={selectedChart-1 < 0}>
+                    <Box sx={{ display: "flex", flexDirection: "row", justifyContent: "space-between" }} alignContent="center">
+                        <IconButton aria-label="back" onClick={handleBackButton} disabled={selectedChart - 1 < 0}>
                             <ArrowBackIosNew fontSize="small" />
                         </IconButton>
 
                         <Typography variant="h6" fontSize={18} color="text.secondary">
-                            Total {charts[selectedChart].name}: {charts[selectedChart].total.toFixed(2)} {charts[selectedChart].unit}
+                            Total {charts[selectedChart].name}: {round(charts[selectedChart].total)} {charts[selectedChart].unit}
                         </Typography>
-                        
-                        <IconButton aria-label="next" onClick={handleNextButton} disabled={selectedChart+1 >= charts.length}>
+
+                        <IconButton aria-label="next" onClick={handleNextButton} disabled={selectedChart + 1 >= charts.length}>
                             <ArrowForwardIos fontSize="small" />
                         </IconButton>
                     </Box>
@@ -155,7 +184,7 @@ const CalculatorGraph = (props: CalculatorGraphProps) => {
                 </Grid>
             }
             <Grid item xs={12}>
-                <CustomPieChart entries={charts[selectedChart].entries} />
+                <CustomPieChart chart={charts[selectedChart]} />
             </Grid>
         </Grid>
     )
